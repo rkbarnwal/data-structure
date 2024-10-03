f771b7eacccde2ffe7c09426e73daed9ccfd9a8d
rkbarnwal <ranjeet.barnwal@gmail.com>
2024-10-03 11:51:25 +0100

    Added method to get the indices of sum of numbers in the array

--- src/main/java/com/rkb/array/ArrayProblem.java
+++ src/main/java/com/rkb/array/ArrayProblem.java
@@ -1,26 +1,25 @@
 package com.rkb.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ArrayProblem {
     public static void main(String[] args) {
-        System.out.println(getIndices(new int[]{1, 3, 5, 7, 9, 2}, 11));
-
+        System.out.println(Arrays.toString(getIndices(new int[]{1, 3, 5, 7, 9, 2}, 11)));
     }
 
     public static int[] getIndices(final int[] numbers, final int sum) {
         Map<Integer, Integer> numMap = new HashMap<Integer, Integer>();
-        int[] indices = null;
         int numToFind;
         for (int i = 0; i < numbers.length; i++) {
             numToFind = sum - numbers[i];
-            if (numMap.containsKey(numToFind)) {
-                return new int[] {numMap.get(numToFind), i};
+            if (numMap.containsKey(numbers[i])) {
+                return new int[]{numMap.get(numbers[i]), i};
             } else {
                 numMap.put(numToFind, i);
             }
         }
-        return indices;
+        return null;
     }
 }
